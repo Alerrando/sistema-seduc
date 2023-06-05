0285412a8a17f7be5a84f6275ce9267a6af344d9
Alerrando Breno <alerrando2@gmail.com>
2023-06-05 01:14:30 -0300

    responsive complete

--- web/src/Components/Aside/index.tsx
+++ web/src/Components/Aside/index.tsx
@@ -1,46 +1,49 @@
-import { BookOpen, Users, PieChart, MenuSquare, AlignJustify } from "lucide-react";
+import { BookOpen, Users, PieChart, MenuSquare, AlignJustify, X } from "lucide-react";
 import Link from "next/link";
 import React, { useState } from "react";
 
-export default function Aside(){
-	const [menu, setMenu] = useState<boolean>(false);
+export default function Aside() {
+  const [menu, setMenu] = useState(false);
 
-	return(
-		<>
-			<AlignJustify className="block sm:hidden" onClick={() => setMenu(true)} />
-			<aside className={`w-1/6 ${menu ? "flex": "hidden sm:flex sm:h-full"} fixed top-0 left-0 flex flex-col gap-8 bg-principal`}>
-				<header className={`w-full ${menu ? "flex": "hidden sm:flex"} flex-col gap-2 items-center justify-start p-[22px] after:w-full after:h-1 after:border-b after:border-[#203F5C]`}>
-					<div className="w-4/5 flex flex-row gap-4 items-center text-white">
-						<BookOpen size={32} />
-						<span className="text-xl">Seduc</span>
-					</div>
-				</header>
+  return (
+    <>
+      <AlignJustify className="block sm:hidden" onClick={() => setMenu(true)} />
+      <aside className={`${menu ? "flex  w-full" : "hidden sm:flex w-1/6"} h-full bg-modal fixed top-0 left-0 flex flex-col gap-8`}>
+        <div className={`${menu ? "w-min" : "w-full"} h-full bg-principal`}>
+          <header className={`w-full ${menu ? "flex" : "hidden sm:flex"} flex-col gap-2 items-center justify-start p-[22px] after:w-full after:h-1 after:border-b after:border-[#203F5C]`}>
+            <div className="w-full flex flex-row gap-4 items-center text-white">
+              <BookOpen className={`w-6 h-6 sm:w-7 sm:h-7`} />
+              <span className="text-xl">Seduc</span>
+              <X className={`w-6 h-6 sm:hidden`} onClick={() => setMenu(false)} />
+            </div>
+          </header>
 
-				<section className={`w-full h-auto ${menu ? "flex": "hidden sm:flex"}`}>
-					<ul className="grid gap-4">
-						<Link href="/" className="w-full block py-3 hover:bg-[#458ACE]">
-							<li className="flex flex-row items-center gap-3 px-5 text-white">
-								<PieChart size={26} />
-								<span>Dashboard</span>
-							</li>
-						</Link>
+          <section className={`w-full h-auto ${menu ? "flex" : "hidden sm:flex"}`}>
+            <ul className="w-full grid gap-4">
+              <Link href="/" className="w-full block py-3 hover:bg-[#458ACE]">
+                <li className="flex flex-row items-center gap-3 px-5 text-white">
+                  <PieChart size={26} />
+                  <span>Dashboard</span>
+                </li>
+              </Link>
 
-						<Link href="/" className="w-full block py-3 hover:bg-[#458ACE]">
-							<li className="flex flex-row items-center gap-3 px-5 text-white">
-								<Users size={26} />
-								<span>Cadastro</span>
-							</li>
-						</Link>
+              <Link href="/" className="w-full block py-3 hover:bg-[#458ACE]">
+                <li className="flex flex-row items-center gap-3 px-5 text-white">
+                  <Users size={26} />
+                  <span>Cadastro</span>
+                </li>
+              </Link>
 
-						<Link href="/" className="w-full block py-3 hover:bg-[#458ACE]">
-							<li className="flex flex-row items-center gap-3 px-5 text-white">
-								<MenuSquare size={26} />
-								<span>Relátorio</span>
-							</li>
-						</Link>
-					</ul>
-				</section>
-			</aside>
-		</>
-	);
+              <Link href="/" className="w-full block py-3 hover:bg-[#458ACE]">
+                <li className="flex flex-row items-center gap-3 px-5 text-white">
+                  <MenuSquare size={26} />
+                  <span>Relatório</span>
+                </li>
+              </Link>
+            </ul>
+          </section>
+        </div>
+      </aside>
+    </>
+  );
 }
--- web/src/Components/Table/index.tsx
+++ web/src/Components/Table/index.tsx
@@ -11,52 +11,50 @@ type TableProps = {
     search: string,
 }
 
-export default function Table(props: TableProps){
+export default function Table(props: TableProps) {
     const { tableHead, editInfo, deleteInfo, infosAll, search } = props;
-    return(
-        <table className="w-full overflow-x-auto">
-            <thead className='w-auto'>
-                <tr>
-                    {tableHead.map(head => <th key={head} scope="col" className="p-1 text-start border border-[#999]">{head}</th>)}
-                </tr>
-            </thead>
 
-            <tbody>
-                {infosAll.filter((register) => Object.values(register.nomeProfessor).join("").toLowerCase().includes(search.toLowerCase()))
-                    .map((info: HorasInfos, index: Key) => {
+    return (
+        <div className="overflow-x-auto">
+            <table className="w-full">
+                <thead>
+                    <tr>
+                        {tableHead.map(head => <th key={head} scope="col" className="p-1 text-start whitespace-nowrap border border-[#999]">{head}</th>)}
+                    </tr>
+                </thead>
 
-                        return (
-                            <tr key={`${index}-${info.nomeProfessor}`}>
-                                <th scope="row" className="p-1 text-start border border-[#999]">{info.id}</th>
-                                <th scope="row" className="p-1 text-start border border-[#999]">{info.nomeProfessor}</th>
-                                <th scope="row" className="p-1 text-start border border-[#999]">{info.horaAulas}</th>
-                                <th scope="row" className="p-1 text-start border border-[#999]">{info.titularidade}</th>
-                                {/* eslint-disable */}
-                                <th scope="row" className="p-1 text-start border border-[#999]">
-                                    {format(new Date(info.diaAula.toString()), "dd/MM/yyyy")}
-                                </th>
-                                {/* eslint-enable */}
-                                <th scope="row" className="p-1 text-start border border-[#999]">{info.escola}</th>
-                                <th scope="row" className="flex items-center justify-between p-1 text-start border-x border-t border-[#999]">
-                                    <div className="w-auto flex flex-row items-center gap-2 px-2 py-1 border border-blue-500 text-blue-500 rounded-lg cursor-pointer hover:bg-blue-500 hover:text-white transition-colors"
-                                        onClick={() => editInfo(info)} 
-                                    >
-                                        <Pencil size={18}/>
-                                        <span>Edit</span>
-                                    </div>
-
-                                    <div 
-                                        className="w-auto flex flex-row items-center gap-2 px-2 py-1 border border-red-500 text-red-500 rounded-lg cursor-pointer hover:bg-red-500 hover:text-white transition-colors"
-                                        onClick={() => deleteInfo(info)}
-                                    >
-                                        <Trash size={18} />
-                                        <span>Delete</span>
-                                    </div>
-                                </th>
-                            </tr>
-                        );
-                    })}
-            </tbody>
-        </table>
+                <tbody>
+                    {infosAll
+                        .filter((register) => Object.values(register.nomeProfessor).join("").toLowerCase().includes(search.toLowerCase()))
+                        .map((info: HorasInfos, index: Key) => {
+                            return (
+                                <tr key={`${index}-${info.nomeProfessor}`}>
+                                    <td className="p-1 text-start whitespace-nowrap border border-[#999]">{info.id}</td>
+                                    <td className="p-1 text-start whitespace-nowrap border border-[#999]">{info.nomeProfessor}</td>
+                                    <td className="p-1 text-start whitespace-nowrap border border-[#999]">{info.horaAulas}</td>
+                                    <td className="p-1 text-start whitespace-nowrap border border-[#999]">{info.titularidade}</td>
+                                    <td className="p-1 text-start whitespace-nowrap border border-[#999]">
+                                        {/* eslint-disable-next-line */}
+                                        <span className='whitespace-nowrap'>{format(new Date(info.diaAula.toString()), "dd/MM/yyyy")}</span>
+                                    </td>
+                                    <td className="p-1 text-start whitespace-nowrap border border-[#999]">{info.escola}</td>
+                                    <td className="p-1 text-start border-x border-t border-[#999]">
+                                        <div className="flex flex-row items-center justify-between">
+                                            <div className="flex items-center gap-2 px-2 py-1 border border-blue-500 text-blue-500 rounded-lg cursor-pointer hover:bg-blue-500 hover:text-white transition-colors" onClick={() => editInfo(info)}>
+                                                <Pencil size={18} />
+                                                <span>Edit</span>
+                                            </div>
+                                            <div className="flex items-center gap-2 px-2 py-1 border border-red-500 text-red-500 rounded-lg cursor-pointer hover:bg-red-500 hover:text-white transition-colors" onClick={() => deleteInfo(info)}>
+                                                <Trash size={18} />
+                                                <span>Delete</span>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                </tbody>
+            </table>
+        </div>
     )
 }
