9e9785a56bd94aabebcfbcf46c6036e88dfbf27b
Alerrando <alerrando2@gmail.com>
2023-06-07 00:48:00 -0300

    add cadastro-escola router

--- server/src/main/java/com/gerenciamentoescolas/server/controllers/CadastroEscolaControllers.java
+++ server/src/main/java/com/gerenciamentoescolas/server/controllers/CadastroEscolaControllers.java
@@ -1,2 +1,37 @@
-package com.gerenciamentoescolas.server.controllers;public class CadastroEscolaControllers {
+package com.gerenciamentoescolas.server.controllers;
+
+import com.gerenciamentoescolas.server.entities.CadastroEscola;
+import com.gerenciamentoescolas.server.services.CadastroEscolaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping(value = "/cadastro-escola")
+public class CadastroEscolaControllers {
+    @Autowired
+    private CadastroEscolaService cadastroEscolaService;
+
+    @GetMapping
+    public List<CadastroEscola> findAll(){
+        List<CadastroEscola> result = cadastroEscolaService.findAll();
+        return result;
+    }
+
+    @PostMapping
+    public CadastroEscola create(@RequestBody CadastroEscola cadastroEscola){
+        return cadastroEscolaService.create(cadastroEscola);
+    }
+
+    @PutMapping
+    public CadastroEscola edit(@PathVariable Integer id, @RequestBody CadastroEscola cadastroEscola){
+        return cadastroEscolaService.edit(id, cadastroEscola);
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable Integer id){
+        cadastroEscolaService.delete(id);
+    }
 }
