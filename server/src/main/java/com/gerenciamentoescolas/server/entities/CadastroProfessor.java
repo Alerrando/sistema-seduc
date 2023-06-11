b7cf68d89c47738b49c68c53cd2ce3c7ce008c47
Alerrando Breno <alerrando2@gmail.com>
2023-06-10 21:44:38 -0300

    add CadastroProfessor entities

---
+++ server/src/main/java/com/gerenciamentoescolas/server/entities/CadastroProfessor.java
@@ -0,0 +1,55 @@
+package com.gerenciamentoescolas.server.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cadastro-professor")
+public class CadastroProfessor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public CadastroProfessor(){
+    }
+
+    public CadastroProfessor(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CadastroProfessor that = (CadastroProfessor) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
