f27a5de856f094b6f3edb5e9437c94197c08cb65
Alerrando Breno <alerrando2@gmail.com>
2023-06-10 22:26:19 -0300

    add cadastro-professor page

---
+++ web/src/app/cadastro-professor/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CadastroProfessor(){
+    return(
+        <main className="w-5/6 ml-auto px-6 overflow-y-auto">
+            <h1>Hello World</h1>
+        </main>
+    )
+}
