a4a01751baf4a3c28be7a8954bf09e469260d3db
Alerrando Breno <alerrando2@gmail.com>
2023-06-14 01:33:47 -0300

    add FormRegisterTeacher folder

---
+++ web/src/Components/Modal/FormRegisterTeacher/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { TeacherInfos } from "../../../../slice";
+import { Plus } from "lucide-react";
+import Input from "@/Components/Input";
+import { useForm } from "react-hook-form";
+
+type FormRegisterTeacherProps = {
+  infosInput: TeacherInfos;
+  submit: (e) => void;
+  setModal: (modal: boolean) => void;
+};
+
+export default function FormRegisterTeacher(props: FormRegisterTeacherProps) {
+  const { infosInput, setModal, submit } = props;
+  const { register, handleSubmit, setValue } = useForm<TeacherInfos>();
+
+  useEffect(() => {
+    if (infosInput.edit !== -1) {
+      setValue("name", infosInput.name);
+    }
+  }, [infosInput.edit]);
+
+  return (
+    <form className="w-full flex flex-col gap-8 py-2 px-4" onSubmit={handleSubmit(submit)}>
+      <div className="w-full flex flex-col gap-3">
+        <Input
+          htmlFor="name"
+          label="Nome do Professor"
+          name="name"
+          placeholder="Ana Laura"
+          register={register}
+          type="text"
+          key={"name-professor-input"}
+        />
+      </div>
+
+      <div className="w-full flex items-center justify-end">
+        <button
+          type="submit"
+          className="flex flex-row items-center gap-2 py-2 px-4 border border-[#22C55E] text-[#22C55E] cursor-pointer rounded-lg group hover:bg-[#22C55E] transition-colors"
+          onClick={() => setModal(true)}
+        >
+          <Plus size={26} className="group-hover:text-white" />
+          <span className="text-lg group-hover:text-white">Cadastro</span>
+        </button>
+      </div>
+    </form>
+  );
+}
