0a9a4655ba24a04eb727bdb4206b3e773095dd59
Alerrando <alerrando2@gmail.com>
2023-06-20 16:47:42 -0300

    changing findProfessoraulas query

--- server/src/main/java/com/gerenciamentoescolas/server/repository/CadastroProfessorRepository.java
+++ server/src/main/java/com/gerenciamentoescolas/server/repository/CadastroProfessorRepository.java
@@ -15,6 +15,6 @@ public interface CadastroProfessorRepository extends JpaRepository<CadastroProfe
     @Query("SELECT p FROM CadastroProfessor p WHERE p.name LIKE %:name%")
     List<CadastroProfessor> filterByName(@Param("name") String name);
 
-    @Query("SELECT p.id, a.horaAulas,a.diaAula,SUM(a.horaAulas) FROM CadastroProfessor p LEFT JOIN CadastroAulas a ON a.cadastroProfessor = :idProfessor WHERE a.diaAula BETWEEN :dataInicial AND :dataFinal GROUP BY p.id, a.horaAulas,a.diaAula")
+    @Query("SELECT p.id, a.hora_aulas,a.dia_aula FROM cadastro_professor p LEFT JOIN cadastro_aulas a ON a.cadastro_professor = :idProfessor WHERE a.dia_aula BETWEEN :dataInicial AND :dataFinal GROUP BY p.id, a.hora_aulas,a.dia_aula")
     List<Object[]> findProfessorAulas(@Param("idProfessor") String idProfessor, @Param("dataInicial") Date dataInicial, @Param("dataFinal") Date dataFinal);
 }
