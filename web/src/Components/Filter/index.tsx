758d9f83168495b040c072a970a40cadefeca786
Alerrando Breno <alerrando2@gmail.com>
2023-06-20 00:10:36 -0300

    strcture filter

--- web/src/Components/Filter/index.tsx
+++ web/src/Components/Filter/index.tsx
@@ -1,7 +1,21 @@
-export export function Filter(){
+import { X } from "lucide-react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../system";
+
+export default function Filter(){
+    const { allInfosTeacher } = useSelector((root: RootState) => root.Slice);
+
     return(
-        <div>
-            
+        <div className="w-screen h-full fixed flex items-center justify-end bg-modal top-0 left-0">
+            <div className="w-[35%] h-full bg-white p-3">
+                <header className="w-full h-auto grid grid-cols-2 items-center justify-between gap-1 after:block after:w-full after:h-1 after:border-b after:border-[#E0E0E0] after:col-span-2">
+                    <h2 className="text-3xl font-bold">Filtro</h2>
+
+                    <X size={36} className="cursor-pointer ml-auto" />
+                </header>
+
+
+            </div>
         </div>
     )
 }
--- web/src/app/relatorio-professor/page.tsx
+++ web/src/app/relatorio-professor/page.tsx
@@ -7,11 +7,11 @@ import { TeacherDTOInfos, changeReportsType } from "../../../slice";
 import TableReports from "../../Components/TableReports";
 import { getReportsTeacher } from "../../api";
 import { useDispatch } from "react-redux";
+import Filter from "../../Components/Filter";
 
 export default function RelatorioProfessor(){
     const [allReportsInfos, setAllReportsInfos] = useState<TeacherDTOInfos[]>([] as TeacherDTOInfos[])
-    const [search, setSearch] = useState("");
-    const tableHead = ["Id", "Nome do Professor", "Quantidade de horas de aulas", "Data das Aulas"];
+    const [filter, setFilter] = useState<boolean>(false)
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -23,14 +23,17 @@ export default function RelatorioProfessor(){
     }, [])
 
     return(
-        <main className="w-5/6 ml-auto px-6 overflow-y-auto">
-            <div className="w-full flex flex-col gap-4 px-6 py-3">
-                <h1 className="text-[42px]">Relatório de Professores</h1>
+        <>
+            <main className="w-5/6 ml-auto px-6 overflow-y-auto">
+                <div className="w-full flex flex-row items-center justify-between gap-4 px-6 py-3">
+                    <h1 className="text-[42px]">Relatório de Professores</h1>
+                    <SlidersHorizontal size={32} className="cursor-pointer" onClick={() => setFilter(!filter)} />
+                </div>
 
-                <CreateHeaderReports setSearch={setSearch} totalRegister={allReportsInfos.length} key="create-header-reports-school" />
-
-                <TableReports infosAll={allReportsInfos} search={search} tableHead={tableHead} key={"table-reports-school"} />
-            </div>
-        </main>
+            </main>
+            {filter ? (
+                <Filter />
+            ) : null}
+        </>
     )
 }
