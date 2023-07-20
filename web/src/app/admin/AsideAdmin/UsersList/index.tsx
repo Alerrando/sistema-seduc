297b69b6f43a90efd6954080bd7bacd7fe615461
Alerrando <alerrando2@gmail.com>
2023-07-19 23:05:04 -0300

    fixing error UserList

--- web/src/app/admin/AsideAdmin/UsersList/index.tsx
+++ web/src/app/admin/AsideAdmin/UsersList/index.tsx
@@ -215,7 +215,6 @@ export default function UsersList(){
                     id,
                     level,
                     edit,
-                    office,
                     ...rest,
                 };
             
