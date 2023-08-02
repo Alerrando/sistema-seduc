ac85398e0668254173b3e1cb8a1dfd406b9b007e
Alerrando <alerrando2@gmail.com>
2023-08-02 15:18:51 -0300

    changing eslintrc

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,76 +1,59 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true,
-		"jest": true,
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
 	},
-	"extends": [
+	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:react/recommended",
 	],
-	"overrides": [
+	overrides: [
 		{
-			"env": {
-				"node": true
+			env: {
+				node: true,
 			},
-			"files": [
-				".eslintrc.{js,cjs}"
-			],
-			"parserOptions": {
-				"sourceType": "script"
-			}
-		}
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaFeatures": {
-			"jsx": true
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
 		},
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"plugins": [
-		"@typescript-eslint",
-		"react",
-		"jsx-a11y",
 	],
-	"rules": {
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint", "react", "jsx-a11y"],
+	rules: {
 		"react/react-in-jsx-scope": "off",
 		"react/prop-types": "off",
 		"no-mixed-spaces-and-tabs": "error",
 		"jsx-a11y/alt-text": [
 			"warn",
 			{
-				"elements": ["img"],
-				"img": ["Image"]
-			}
+				elements: ["img"],
+				img: ["Image"],
+			},
 		],
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"windows"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
 		"jsx-a11y/aria-props": "warn",
 		"jsx-a11y/aria-proptypes": "warn",
 		"jsx-a11y/aria-unsupported-elements": "warn",
 		"jsx-a11y/role-has-required-aria-props": "warn",
-		"jsx-a11y/role-supports-aria-props": "warn"
+		"jsx-a11y/role-supports-aria-props": "warn",
 	},
-	"settings": {
-		"react": {
-			"version": "detect"
-		}
-	}
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
 };
+  
